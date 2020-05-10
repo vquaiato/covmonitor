c1f5d7b295420de235046960b5baac070e4af5cb
Vinicius Quaiato <vinicius.quaiato@gmail.com>
2020-05-10 18:34:04 -0300

    transformar dados no formato que queremos

--- lib/cov_monitor.ex
+++ lib/cov_monitor.ex
@@ -4,17 +4,31 @@ defmodule CovMonitor do
   """
 
   # escrever testes
-  # retornar dados em um formato que queiramos
+  # esta função tem muitas responsabilidades, não deveria
   def fazer_chamada_api(url) do
     case HTTPoison.get(url) do
-      {:ok, resposta} -> parse_do_json(resposta)
-      {_status, erro} -> IO.inspect erro
+      {:ok, resposta} -> parse_do_json(resposta) |> transformar_dados
+      {_status, erro} -> IO.inspect(erro)
     end
   end
 
+  #mover o matching do body direto pro heading da função
   defp parse_do_json(resposta_http) do
     %{:body => corpo} = resposta_http
     casos_brasil = Jason.decode!(corpo)
     casos_brasil
+  end
+
+  defp transformar_dados(casos_brasil_da_api) do
+    Enum.map(casos_brasil_da_api, fn mapa ->
+      %{
+        "confirmados" => mapa["Confirmed"],
+        "ativos" => mapa["Active"],
+        "óbitos" => mapa["Deaths"],
+        "recuperados" => mapa["Recovered"],
+        "país" => "Brazil",
+        "data" => mapa["Date"] |> DateTime.from_iso8601()
+      }
+    end)
   end
 end
--- test/cov_monitor_test.exs
+++ test/cov_monitor_test.exs
@@ -2,7 +2,10 @@ defmodule CovMonitorTest do
   use ExUnit.Case
   doctest CovMonitor
 
-  test "greets the world" do
-    assert CovMonitor.hello() == :world
+  test "transforma mapa da api para nosso dado" do
+    assert :ok == :error
+  end
+  test "transforma uma lista de mapas da api em uma lista de nossos dados" do
+    assert :ok == :error
   end
 end
