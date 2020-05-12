767ddef5bd4f0007ee3e8fd88e9672c7828fb39c
Vinicius Quaiato <vinicius.quaiato@gmail.com>
2020-05-11 21:26:47 -0300

    adicionado mais um teste para adapter

--- test/conv_monitor_adapter_test.exs
+++ test/conv_monitor_adapter_test.exs
@@ -42,4 +42,38 @@ defmodule CovMonitor.Adapter.Test do
     }
     assert Adapter.de_casos_api_para_casos_interno([@dado_api]) == [dado_esperado]
   end
+
+  test "transforma uma lista com mais de um dado da api em uma lista com mais de um dado interno" do
+    dado_esperado = [%{
+      "ativos" => 1,
+      "óbitos" => 0,
+      "confirmados" => 2,
+      "recuperados" => 10,
+      "data" => "06/05/2020",
+      "país" => "Brazil"
+    }, %{
+      "ativos" => 2,
+      "óbitos" => 2,
+      "confirmados" => 2,
+      "recuperados" => 2,
+      "data" => "10/05/2020",
+      "país" => "Brazil"
+    }]
+
+    dados_api = [@dado_api, %{
+      "Active" => 2,
+      "Deaths" => 2,
+      "Confirmed" => 2,
+      "Recovered" => 2,
+      "Date" => "2020-05-10T00:00:00Z",
+      "Country" => "Brazil",
+      "City" => "",
+      "CityCode" => "",
+      "CountryCode" => "BR",
+      "Lat" => "-14.24",
+      "Lon" => "-51.93",
+      "Province" => ""
+    }]
+    assert Adapter.de_casos_api_para_casos_interno(dados_api) == dado_esperado
+  end
 end
